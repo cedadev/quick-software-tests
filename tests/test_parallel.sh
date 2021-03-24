94ac42964b5ad8c341a2b0e21960de91c85a03ff
Alan Iwi <alan.iwi@stfc.ac.uk>
2021-03-24 11:57:09 +0000

    fix parallel test

--- tests/test_parallel.sh
+++ tests/test_parallel.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-out=$((echo foo ; echo bar) | parallel ./_test_parallel_helper.sh 2>/dev/null)
+out=$((echo foo ; echo bar) | parallel ./_test_parallel_helper.sh 2>/dev/null | fmt -80)
 
 if [ "$out" = "oof rab" ]
 then
     echo parallel test passed
     exit 0
 else
-    echo parallel test passed
+    echo parallel test failed
     exit 1
 fi
 
