e9098cdfae7b7768528d82d485f20c0ce1a7375d
Alan Iwi <alan.iwi@stfc.ac.uk>
2021-11-02 09:29:07 +0000

    remove irrelevant stuff in R test_all.sh copied from jaspy test script

--- r-tests/test_all.sh
+++ r-tests/test_all.sh
@@ -13,16 +13,6 @@ fi
 export PLOT_DIR=../compare/images/test
 mkdir -p $PLOT_DIR
 
-export DATA_DIR=$(python -c 'import os, iris_sample_data as isd; sample_data_dir = os.path.join(os.path.dirname(isd.__file__), "sample_data"); print(sample_data_dir)')
-
-
-if [ ! -d $DATA_DIR ]; then
-    echo "[ERROR] Cannot get example data so NOT running tests."
-    exit
-fi
-
-#python test_cfplot.py
-
 function test_run {
 
     cmd=$1
