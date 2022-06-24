24c724fdab0a57501c99d25000fe99d6b5b1b2dd
Alan Iwi <alan.iwi@stfc.ac.uk>
2022-06-24 16:16:18 +0100

    update eccodes test

--- tests/test_eccodes.py
+++ tests/test_eccodes.py
@@ -1,4 +1,11 @@
 import eccodes
 
-assert(type(eccodes.GribFile) == type)
+print('running eccodes test')
 
+gid = eccodes.codes_grib_new_from_file(open("testdata/gfs20220525132602953.grb"))
+values = eccodes.codes_get_values(gid)
+
+assert len(values) == 264
+assert 102310 < values[1] < 102311
+
+print('ran eccodes test')
