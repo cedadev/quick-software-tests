5b84c5339db401d09ad0174e433c6e5933550e6e
Alan Iwi <alan.iwi@stfc.ac.uk>
2024-03-02 20:00:31 +0000

    fix invalid SQL in test_sqlite (how did this work before?)

--- tests/test_sqlite.sh
+++ tests/test_sqlite.sh
@@ -4,8 +4,8 @@ db=test.db
 
 rm -f $db 
 echo 'create table foo(id int primary key not null, blah char(50) not null);' | sqlite3 $db
-echo 'insert into foo values (1, "hello");' | sqlite3 $db
-echo 'insert into foo values (2, "world");' | sqlite3 $db
+echo "insert into foo values (1, 'hello');" | sqlite3 $db
+echo "insert into foo values (2, 'world');" | sqlite3 $db
 
 value=$(echo 'select blah from foo where id=2;'  | sqlite3 $db)
 
