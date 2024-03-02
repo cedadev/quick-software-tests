dd5bc354a43b1603c64342855e8ea121a6c53696
Alan Iwi <alan.iwi@stfc.ac.uk>
2024-03-02 20:14:52 +0000

    update cdo test (an expected line of output now not shown when stdout to a pipe, unless -v specified)

--- tests/test_PT_cdo.py
+++ tests/test_PT_cdo.py
@@ -2,7 +2,7 @@
 from _pt_common import _run, _setup, _fpath, _check_output
 
 def test_cdo_infov():
-    cmd = 'cdo infov {}'.format(_fpath('A1B_north_america.nc'))
+    cmd = 'cdo -v infov {}'.format(_fpath('A1B_north_america.nc'))
     _check_output(cmd, 'Processed 435120 values from 1 variable over 240 timesteps')
 
 
