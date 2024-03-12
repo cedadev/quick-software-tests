d5f7975c74ddcb354dd20c4162c9a64861cafc0f
Alan Iwi <alan.iwi@stfc.ac.uk>
2024-03-12 09:46:15 +0000

    seems that cdo no longer permits infov - has to be infon - so update

--- tests/test_PT_cdo.py
+++ tests/test_PT_cdo.py
@@ -1,8 +1,8 @@
 
 from _pt_common import _run, _setup, _fpath, _check_output
 
-def test_cdo_infov():
-    cmd = 'cdo -v infov {}'.format(_fpath('A1B_north_america.nc'))
+def test_cdo_infon():
+    cmd = 'cdo -v infon {}'.format(_fpath('A1B_north_america.nc'))
     _check_output(cmd, 'Processed 435120 values from 1 variable over 240 timesteps')
 
 
