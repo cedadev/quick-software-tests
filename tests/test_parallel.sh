2ccae50294bbef6dd929c9b2eab5f47bbc00cf6d
Alan Iwi <alan.iwi@stfc.ac.uk>
2024-03-12 16:36:27 +0000

    add -k flag in parallel test or order cannot be guaranteed

--- tests/test_parallel.sh
+++ tests/test_parallel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-out=$((echo foo ; echo bar) | parallel ./_test_parallel_helper.sh 2>/dev/null | fmt -80)
+out=$((echo foo ; echo bar) | parallel -k ./_test_parallel_helper.sh 2>/dev/null | fmt -80)
 
 if [ "$out" = "oof rab" ]
 then
