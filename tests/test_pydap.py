9ae5bd0b8270a20e881b680582d998297705266d
Alan Iwi <alan.iwi@stfc.ac.uk>
2024-05-08 23:12:24 +0100

    change endpoint for pydap test (test.opendap.org not responding)

--- tests/test_pydap.py
+++ tests/test_pydap.py
@@ -1,10 +1,22 @@
+import numpy as np
 from pydap.client import open_url
 
 print("running pydap test")
 
-dataset = open_url('http://test.opendap.org/dap/data/nc/coads_climatology.nc')
-sst = dataset['SST']
+# dataset = open_url('http://test.opendap.org/dap/data/nc/coads_climatology.nc')
+# sst = dataset['SST']
+# 
+# assert sst.shape == (12, 90, 180)
+# val, = sst[1, 40, 70].array
+# assert 29 < val < 30
+#
 
-assert sst.shape == (12, 90, 180)
-val, = sst[1, 40, 70].array
-assert 29 < val < 30
+
+dataset = open_url('https://psl.noaa.gov/thredds/dodsC/Datasets/noaa.oisst.v2/sst.mnmean.nc')
+sst = dataset['sst']
+
+assert sst.shape == (494, 180, 360)
+
+val = np.array(sst[1, 40, 70]).flatten()[0]
+assert val == 922
+
