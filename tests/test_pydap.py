f5c30fc17c247939161e30aa364bedfbf8a02797
Alan Iwi <alan.iwi@stfc.ac.uk>
2024-08-16 15:15:38 +0100

    update pydap test to try two different remote datasets (as different servers may time out)

--- tests/test_pydap.py
+++ tests/test_pydap.py
@@ -3,20 +3,26 @@ from pydap.client import open_url
 
 print("running pydap test")
 
-# dataset = open_url('http://test.opendap.org/dap/data/nc/coads_climatology.nc')
-# sst = dataset['SST']
-# 
-# assert sst.shape == (12, 90, 180)
-# val, = sst[1, 40, 70].array
-# assert 29 < val < 30
-#
+try:
+    dataset = open_url('http://test.opendap.org/dap/data/nc/coads_climatology.nc',
+                       timeout=10)
 
+    print('Using COADS data for test')
+    sst = dataset['SST']
+    assert sst.shape == (12, 90, 180)
+    val, = sst[1, 40, 70].array
+    assert 29 < val < 30
 
-dataset = open_url('https://psl.noaa.gov/thredds/dodsC/Datasets/noaa.oisst.v2/sst.mnmean.nc')
-sst = dataset['sst']
+except Exception as exc:
 
-assert sst.shape == (494, 180, 360)
+    if str(exc) != 'Timeout':
+        raise
 
-val = np.array(sst[1, 40, 70]).flatten()[0]
-assert val == 922
+    dataset = open_url('https://psl.noaa.gov/thredds/dodsC/Datasets/noaa.oisst.v2/sst.mnmean.nc',
+                       timeout=20)
+    print('Using NOAA OISST data for test')
+    sst = dataset['sst']
+    assert sst.shape == (494, 180, 360)
+    val = np.array(sst[1, 40, 70]).flatten()[0]
+    assert val == 922
 
